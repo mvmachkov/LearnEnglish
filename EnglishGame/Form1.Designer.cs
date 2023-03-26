9953b656585b6d1c1ddef86f7d66461cf4c2f9f0
mvmachkov <mvmachkov@gmail.com>
2023-03-26 20:10:12 +0300

    change some font and sizes

--- EnglishGame/Form1.Designer.cs
+++ EnglishGame/Form1.Designer.cs
@@ -29,24 +29,38 @@
         private void InitializeComponent()
         {
             this.EnglishWordLabel = new System.Windows.Forms.Label();
+            this.userInputBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // EnglishWordLabel
             // 
             this.EnglishWordLabel.AutoSize = true;
-            this.EnglishWordLabel.Location = new System.Drawing.Point(350, 150);
+            this.EnglishWordLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnglishWordLabel.Location = new System.Drawing.Point(339, 157);
             this.EnglishWordLabel.Name = "EnglishWordLabel";
-            this.EnglishWordLabel.Size = new System.Drawing.Size(43, 16);
+            this.EnglishWordLabel.Size = new System.Drawing.Size(62, 21);
             this.EnglishWordLabel.TabIndex = 0;
             this.EnglishWordLabel.Text = "AAAA";
             this.EnglishWordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // userInputBox
+            // 
+            this.userInputBox.Font = new System.Drawing.Font("MV Boli", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInputBox.Location = new System.Drawing.Point(212, 249);
+            this.userInputBox.Name = "userInputBox";
+            this.userInputBox.Size = new System.Drawing.Size(353, 37);
+            this.userInputBox.TabIndex = 1;
+            this.userInputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.userInputBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.userInputBox_PreviewKeyDown);
             // 
             // LearnEnglish
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(792, 464);
+            this.Controls.Add(this.userInputBox);
             this.Controls.Add(this.EnglishWordLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "LearnEnglish";
             this.Text = "LearnEnglish";
             this.ResumeLayout(false);
@@ -57,6 +71,7 @@
         #endregion
 
         public System.Windows.Forms.Label EnglishWordLabel;
+        private System.Windows.Forms.TextBox userInputBox;
     }
 }
 
--- EnglishGame/Form1.cs
+++ EnglishGame/Form1.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Threading;
 using System.IO;
+using System.Linq;
+//using System.Threading;
+using System.Windows.Forms;
 
 namespace EnglishGame
 
@@ -16,69 +12,197 @@ namespace EnglishGame
 {
     public partial class LearnEnglish : Form
     {
+        public static Library labrary = new Library();
+        private static int userInputX;
+        private Timer colorTimer = new Timer();
+        private Timer movePositionTimer = new Timer();
+        private static int colorCount = 0;
+        private static int moveCount = 0;
+        private static int sizeOfText = 9;
+
         public LearnEnglish()
         {
             InitializeComponent();
-            var labrary = new Library();
-           // EnglishWordLabel.Text = labrary.EnglishWordsStack.Pop();
-
-
-
-            
-
-
+            userInputX = userInputBox.Left;
+            EnglishWordLabel.Text = labrary.EnglishWordsStack.Pop();
+            InitializeTicks();
         }
 
-
-    }
-    public class Library
-    {
-        private Dictionary<string, List<string>> dictionary;
-        private Stack<string> stackWords;
-        private static string path = @"C:\Users\Milen Machkov\OneDrive\Работен плот\aEmpty\03.EN\wordsAlignment.txt";
-
-
-        public Library()
-        {
-            // GenerateDictionary();
-            //in the stack
-            //GenerateTheWords();
-            
-        }
-        public Stack<string> EnglishWordsStack
-        {
-            get
-            {
-                return this.stackWords;
-            }
-        }
-        private void GenerateDictionary()
-        {
-            this.dictionary = new Dictionary<string, List<string>>();
-            StreamReader reader = new StreamReader(path);
-            var line = string.Empty;
-            string enWord = string.Empty;
-            List<string> bgWords = new List<string>();
-            using (reader)
-            {
-                line = reader.ReadLine();
-
-
-            }
-
-
-        }
-        private void GenerateTheWords()
-        {
-            this.stackWords = new Stack<string>();
-            foreach (var item in dictionary.Keys)
-            {
-                this.stackWords.Push(item);
-            }
-        }
-        
 
        
+        private void ShakeWrong()
+        {
+           
+            movePositionTimer.Start();
+
+
+        }
+        private void MovePositionTimer_Tick(object sender, EventArgs e)
+        {
+            if (moveCount <= 3)
+            {
+                if (moveCount % 2 == 0)
+                {
+                    userInputBox.Left += 3;
+
+                }
+                else
+                {
+                    userInputBox.Left -= 6;
+                }
+                moveCount++;
+            }
+            else
+            {
+                userInputBox.Left = userInputX;
+                movePositionTimer.Stop();
+                moveCount = 0;
+            }
+
+        }
+
+      
+        private void InitializeTicks()
+        {
+            colorTimer.Interval = 100;
+            colorTimer.Tick += new EventHandler(ColorTimer_Tick);
+            movePositionTimer.Interval = 50;
+            movePositionTimer.Tick += new EventHandler(MovePositionTimer_Tick);
+        }
+        private void CorrectAnimation()
+        {
+            
+            colorTimer.Start();
+
+        }
+        private void ColorTimer_Tick(object sender, EventArgs e)
+        {
+            EnglishWordLabel.Font = new Font("Arial Rounded MT Bold", sizeOfText);
+            sizeOfText++;
+            if (colorCount <= 5)
+            {
+                if(colorCount % 2 == 0)
+                {
+                    EnglishWordLabel.ForeColor= Color.Green;
+                }
+                else
+                {
+                    EnglishWordLabel.ForeColor = Color.Black;
+                }
+                colorCount++;
+            }
+            else
+            {
+                userInputBox.ForeColor = Color.Black;
+                EnglishWordLabel.Text = labrary.EnglishWordsStack.Pop();
+                colorCount = 0;
+                sizeOfText = 9;
+                colorTimer.Stop();
+            }
+
+        }
+
+        private void userInputBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (labrary.CheckTheWord(EnglishWordLabel.Text, userInputBox.Text))
+                {
+                    CorrectAnimation();
+                    
+                   
+                    CleanInputBox();
+                }
+                else
+                {
+                    ShakeWrong();
+                }
+
+            }
+        }
+
+
+
+        private void CleanInputBox()
+        {
+            userInputBox.Clear();
+        }
+    }
+    
+}
+public class Library
+{
+    private Dictionary<string, List<string>> dictionary;
+    private Stack<string> stackWords;
+    //todo still doesnt work
+    private static string path = @"C:\Users\mvmac\source\repos\LearnEnglish\ProccesData\data\WordsAlignment.txt";
+
+
+
+    public Library()
+    {
+        GenerateDictionary();
+        //in the stack
+        GenerateTheWords();
 
     }
+    public Stack<string> EnglishWordsStack
+    {
+        get
+        {
+            return this.stackWords;
+        }
+    }
+    private void GenerateDictionary()
+    {
+        this.dictionary = new Dictionary<string, List<string>>();
+        StreamReader reader = new StreamReader(path);
+        var line = string.Empty;
+        string enWord = string.Empty;
+        List<string> bgWords = new List<string>();
+        int equallyIndex = 0;
+        char[] separators = new char[1] { ',' };
+
+        using (reader)
+        {
+            while (true)
+            {
+                line = reader.ReadLine();
+                if (line == null) break;
+                equallyIndex = line.IndexOf('=');
+                enWord = line.Substring(0, equallyIndex);
+                string newSTR = line.Substring(equallyIndex + 1);
+                bgWords = newSTR.Split(separators, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (this.dictionary.ContainsKey(enWord))
+                {
+                    continue;
+                }
+                this.dictionary.Add(enWord, bgWords);
+            }
+        }
+    }
+    public bool CheckTheWord(string enWord, string bgWord)
+    {
+        List<string> answers = this.dictionary[enWord];
+        foreach (var answer in answers)
+        {
+            if (answer == bgWord) return true;
+        }
+        return false;
+
+    }
+    private void GenerateTheWords()
+    {
+        this.stackWords = new Stack<string>();
+        foreach (var item in dictionary.Keys)
+        {
+            this.stackWords.Push(item);
+        }
+    }
+
+
+
+
 }
+
